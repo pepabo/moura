3871e083c3d5150bba14318a0baa54128c8cd008
Yuki Koya <ykky@pepabo.com>
2022-09-15 16:46:47 +0900

    bundle gem moura

---
+++ lib/moura.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "moura/version"
+
+module Moura
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/moura/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Moura
+  VERSION = "0.1.0"
+end
